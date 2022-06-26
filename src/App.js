f250f7db393d41c45c62613286dfffc455aadd72
ramec0 <104471885+ramec0@users.noreply.github.com>
2022-06-26 13:22:13 -0400

    added 2nd change and comitted after using git add

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit(ed1) <code>src/App.js</code> and save to reload.
+          Edit(ed(22)) <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
